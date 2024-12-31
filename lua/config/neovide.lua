fdac137f5b9eedbf547bafbb97103da290d43a8a
danielosw <65432065+danielosw@users.noreply.github.com>
2024-12-30 18:37:13 -0600

    add ripsubstatute and sciscors

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -4,11 +4,6 @@ local o = vim.o
 g.neovide_theme = "auto"
 g.neovide_no_idle = true
 g.neovide_cursor_vfx_mode = "railgun"
-keymap.set("v", "<D-c>", '"+y') -- Copy
-keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 if Windows then
 	g.neovide_title_background_color =
 		string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -28,6 +28,47 @@ return {
 		opts = {},
 	},
 	{
+		"chrisgrieser/nvim-rip-substitute",
+		cmd = "RipSubstitute",
+		opts = {},
+		keys = {
+			{
+				"<leader>fs",
+				function()
+					require("rip-substitute").sub()
+				end,
+				mode = { "n", "x" },
+				desc = " rip substitute",
+			},
+		},
+	},
+	{
+		"chrisgrieser/nvim-scissors",
+		dependencies = "nvim-telescope/telescope.nvim",
+		keys = {
+			{
+				"<leader>se",
+				function()
+					require("scissors").editSnippet()
+				end,
+				mode = { "n" },
+				desc = "Snippet: Edit",
+			},
+			{
+				"<leader>sa",
+				function()
+					require("scissors").addNewSnippet()
+				end,
+				mode = { "n", "x" },
+				desc = "Snippet: Add",
+			},
+		},
+		opts = {
+
+			snippetDir = vim.fn.stdpath("config") .. "/snippets",
+		},
+	},
+	{
 		"stevearc/conform.nvim",
 		event = { "BufWritePre" },
 		cmd = { "ConformInfo" },
