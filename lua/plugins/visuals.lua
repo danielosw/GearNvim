04e5cf5d667d3d47b9781a45cf9565224124a461
danielosw <65432065+danielosw@users.noreply.github.com>
2025-02-13 23:12:08 -0600

    dressing -> snacks

--- lua/plugins/visuals.lua
+++ lua/plugins/visuals.lua
@@ -144,8 +144,28 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 	},
 	{
-		"stevearc/dressing.nvim",
-		opts = {},
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			bigfile = { enabled = true },
+			dashboard = { enabled = false },
+			explorer = { enabled = false },
+			indent = { enabled = false },
+			input = { enabled = true },
+			picker = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = not vim.g.neovide },
+			statuscolumn = { enabled = false },
+			words = { enabled = true },
+			image = { enabled = true },
+		},
 	},
 	{
 		"nvim-tree/nvim-tree.lua",
