ff2c8c1eb020b5296254b19c1e563907a4d4ffd5
danielosw <65432065+danielosw@users.noreply.github.com>
2025-05-07 19:56:45 -0500

    bump

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -106,4 +106,10 @@ return {
 			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 		end,
 	},
+	{
+		"nvim-neorg/neorg",
+		lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+		version = "*", -- Pin Neorg to the latest stable release
+		config = true,
+	},
 }
