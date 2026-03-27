7d7429a7a134186bca97708687f98e8a84e48f71
danielosw <65432065+danielosw@users.noreply.github.com>
2026-03-27 11:19:38 -0500

    add quote

--- lua/lib/quotes.lua
+++ lua/lib/quotes.lua
@@ -4,22 +4,24 @@ This file is a list of quotes to randomly load on start
 --
 local Inspire = true
 local Comedy = true
---[[
-Vaugly inspiring
---]]
+local Other = true
+--Vaugly inspiring
+
 local inspire = {
 	"Strive for progress.",
 	"Nothing about us without us.",
 	"Even the waves of fate can break upon the shores of will.",
 	"Should the lights go out and all laws flee, you will get where you are going.",
 }
---[[
-Comedy
---]]
+--Comedy
+
 local funny = {
 	"I use Arch btw",
 }
-
+-- other
+local other = {
+	"One day I will go north to the end of the world, stained with the inks of the violet undernight, and there will I burn",
+}
 -- Build the quotes table
 Quotes = {}
 if Inspire then
@@ -27,4 +29,7 @@ if Inspire then
 end
 if Comedy then
 	vim.list_extend(Quotes, funny)
+end
+if Other then
+	vim.list_extend(Quotes, other)
 end
