b11fb46b2d8ad593c7937d841265c69994bd9d5d
danielosw <65432065+danielosw@users.noreply.github.com>
2026-04-17 12:17:06 -0500

    make bubilds async

--- lua/lib/autocmds.lua
+++ lua/lib/autocmds.lua
@@ -1,22 +1,27 @@
+---@param obj vim.SystemCompleted
+local function buildcallback(obj)
+	if obj.code == 0 then
+		vim.notify("Building done", vim.log.levels.INFO)
+	else
+		vim.notify("Building failed with error code " .. obj.code .. " and message " .. obj.stderr,
+			vim.log.levels.ERROR)
+	end
+end
+
 -- setup autocmds
 ---@param ev vim.api.keyset.events
 local hooks = function(ev)
 	local name, kind = ev.data.spec.name, ev.data.kind
 	if name == "telescope-fzf-native.nvim" and (kind == "install" or kind == "update") then
-		local shell = Windows and "pwsh.exe" or "sh"
+		local shell = Globals.Windows and "pwsh.exe" or "sh"
 		vim.system({
 			shell,
 			"-c",
-			"cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-		}, { cwd = ev.data.path }):wait()
+			"cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+		}, { cwd = ev.data.path }, buildcallback)
 	elseif name == "blink.cmp" and (kind == "install" or kind == "update") then
 		vim.notify("Building blink.cmp", vim.log.levels.INFO)
-		local obj = vim.system({ "cargo", "build", "--release" }, { cwd = ev.data.path }):wait()
-		if obj.code == 0 then
-			vim.notify("Building blink.cmp done", vim.log.levels.INFO)
-		else
-			vim.notify("Building blink.cmp failed", vim.log.levels.ERROR)
-		end
+		local _ = vim.system({ "cargo", "build", "--release" }, { cwd = ev.data.path }, buildcallback)
 	end
 end
 local callback = function()
